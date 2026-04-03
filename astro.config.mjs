ccc92425875d4a4315eead221a87335a8ec52171
Hugo Landau <hl@messier42.com>
2026-04-02 18:21:04 -0700

    Fix website path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import {defineConfig} from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://messier-42.github.io',
-  base: '/cabe-website',
+  base: '/cabe',
   trailingSlash: 'ignore',
 });
